e4c6ee872cf7d3de0dadab577dd39eb2eb9c15c0
rafaelgaln <rafael.galn@educa.madrid.org>
2024-04-02 09:19:59 +0200

    Métodos de la clase Coche.

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -19,4 +19,32 @@ public class Coche {
         gasolinaRestante = 0;
         cuentakilometros = 0;
     }
+
+    //Métodos
+    public void imprimirInfo() {
+        System.out.println("Marca: " + marca);
+        System.out.println("Modelo: " + modelo);
+        System.out.println("Matrícula: " + matricula);
+        System.out.println("Color: " + color);
+        System.out.println("Gasolina restante (Litros): " + gasolinaRestante);
+        System.out.println("Cuentakilómetros: " + cuentakilometros);
+    }
+    public boolean conducirCoche (double kilometrosRecorridos) {
+        System.out.println("Vamos a conducir el " + marca + " " + modelo);
+        if (gasolinaRestante < (kilometrosRecorridos*0.06)) {
+            System.out.println("No hay suficiente gasolina para conducir");
+            return false;
+        }
+        cuentakilometros += kilometrosRecorridos;
+        gasolinaRestante -= (kilometrosRecorridos*0.06);
+        System.out.println("Has conducido tu " + marca + " " + modelo + " " + kilometrosRecorridos + " kilómetros");
+        System.out.println("- Ha consumido " + (kilometrosRecorridos*0.06) + " litros de combustible");
+        return true;
+    }
+
+    public boolean repostarCoche (double recargaLitrosGasolina) {
+        gasolinaRestante += recargaLitrosGasolina;
+        System.out.println("Has repostado tu " + marca + " " + modelo + " " + recargaLitrosGasolina + " litros de gasolina");
+        return true;
+    }
 }
--- src/programa/Main.java
+++ src/programa/Main.java
@@ -1,8 +1,17 @@
 package programa;
 
+import modelo.Coche;
+import modelo.Concesionario;
 public class Main {
 
     public static void main(String[] args) {
 
+        Concesionario concesionarioCitroen = new Concesionario ("SuperCoches", "Citroen", "Leganes");
+        Coche CitroenC4 = new Coche ("Citroen", "C4", "Rojo", "6745JUP");
+        Coche RenaultClio = new Coche ("Renault", "Clio", "Amarillo", "9746LOK");
+
+        CitroenC4.conducirCoche(10);
+        CitroenC4.repostarCoche(50);
+        CitroenC4.conducirCoche(10);
     }
 }
