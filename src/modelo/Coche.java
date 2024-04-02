574077bf666987b9e3f461f75225563e1c327e97
rafaelgaln <rafael.galn@educa.madrid.org>
2024-04-02 09:05:07 +0200

    Atributos y Constructores de Coche y Concesionario.

--- src/modelo/Coche.java
+++ src/modelo/Coche.java
@@ -1,4 +1,22 @@
 package modelo;
 
 public class Coche {
+    //Atributos Fijos
+    protected final String marca;
+    protected final String modelo;
+    protected final String matricula;
+
+    //Atributos Variables
+    protected String color;
+    protected double gasolinaRestante;
+    protected double cuentakilometros;
+
+    //Constructor
+    public Coche (String marca, String modelo, String matricula, String color) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.matricula = matricula;
+        gasolinaRestante = 0;
+        cuentakilometros = 0;
+    }
 }
--- src/modelo/Concesionario.java
+++ src/modelo/Concesionario.java
@@ -1,4 +1,22 @@
 package modelo;
 
+import java.util.ArrayList;
+
 public class Concesionario {
+
+    //Atributos Fijos
+    protected final String nombre;
+    protected final String marcaConcesionario;
+    protected final String ubicacion;
+
+    //Atributos Variables
+    protected ArrayList<Coche> inventarioCoches;
+
+    //Constructor
+    public Concesionario (String nombre, String marcaConcesionario, String ubicacion) {
+        this.nombre = nombre;
+        this.marcaConcesionario = marcaConcesionario;
+        this.ubicacion = ubicacion;
+        this.inventarioCoches = new ArrayList<Coche>();
+    }
 }
